42cbaea3a0234f614b8d62e18ec1dd6210567ee8
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2021-11-27 12:39:30 -0600

    Home task 2

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import { User } from './types';
+import UserList from './components/UserList';
+import AddUserForm from './components/AddUserForm';
+
+const initUsers: User[] =
+    [
+        {
+            id: 0,
+            firstName: "Ivan",
+            lastName: "Ivanov",
+            phone: "(050) 653-9041",
+        },
+        {
+            id: 1,
+            firstName: "Petro",
+            lastName: "Petrov",
+            phone: "(097) 653-9041",
+        },
+    ];
+
+const App = () => {
+    const [users, setUsers] = React.useState<User[]>(initUsers);
+    return (
+        <div>
+            <h2>Home Task #2</h2>
+            <AddUserForm onSubmitFunc={newUser => setUsers([...users, newUser])} />
+            <UserList userList={users} removeUser={id => setUsers(users.filter(user => user.id !== id))} />
+        </div>
+    );
+}
+
+export default App;
